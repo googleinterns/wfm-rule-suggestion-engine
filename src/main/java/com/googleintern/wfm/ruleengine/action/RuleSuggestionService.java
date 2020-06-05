d75e4e831c25ea988374f7996e07e6860c8cbc93
Vivian Han <qintonghan@google.com>
2020-06-05 21:39:01 +0000

    Initial Contribution
    Create UserPoolAssignment class, PoolAssignment class, Filter class, and
    Rule class.
    
    Set up BUILD.bazel and WORKSPACE files.
    
    Signed-off-by: Vivian Han <qintonghan@google.com>

--- src/main/java/com/googleintern/wfm/ruleengine/action/RuleSuggestionService.java
+++ src/main/java/com/googleintern/wfm/ruleengine/action/RuleSuggestionService.java
@@ -1,4 +1,8 @@
+<<<<<<< HEAD:src/main/java/com/googleintern/wfm/ruleengine/action/RuleSuggestionService.java
 package src.main.java.com.googleintern.wfm.ruleengine.action;
+=======
+package java.src.com.googleintern.wfm.ruleengine.action;
+>>>>>>> 8be7a5e... Initial Contribution:java/src/com/googleintern/wfm/ruleengine/action/RuleSuggestionService.java
 
 public interface RuleSuggestionService {
 
