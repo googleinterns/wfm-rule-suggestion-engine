19574bb6cd473243e5b3b2a49922fc94b5603ae3
qintonghan98 <65258617+qintonghan98@users.noreply.github.com>
2020-06-12 16:07:27 -0400

    Set up Bazel Environment and Create UserPoolAssignment/Rule classes (#13)
    * Initial Contribution
    
    Create UserPoolAssignment class, PoolAssignment class, Filter class, and
    Rule class.
    
    Set up BUILD.bazel and WORKSPACE files.
    
    Signed-off-by: Vivian Han <qintonghan@google.com>

---
+++ src/main/java/com/googleintern/wfm/ruleengine/model/RuleModel.java
@@ -0,0 +1,36 @@
+package src.main.java.com.googleintern.wfm.ruleengine.model;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * RuleModel class is used to store detailed information about a newly generated rule.
+ */
+@AutoValue
+public abstract class RuleModel {
+    public abstract long workforceId();
+    public abstract long workgroupId();
+    public abstract long casePoolId();
+    public abstract ImmutableSet<Long> permissionSetIds();
+    public abstract ImmutableList<ImmutableSet<FilterModel>> filters();
+
+    public static Builder builder() {
+        return new AutoValue_RuleModel.Builder();
+    }
+
+    /**
+     * Builder class is used to set variables and create an instance for RuleModel class.
+     */
+    @AutoValue.Builder
+    public abstract static class Builder{
+        public abstract Builder setWorkforceId(long workforceId);
+        public abstract Builder setWorkgroupId(long workgroupId);
+        public abstract Builder setCasePoolId(long casePoolId);
+        public abstract Builder setPermissionSetIds(Set<Long> permissionSetIds);
+        public abstract Builder setFilters(List<ImmutableSet<FilterModel>> filters);
+        public abstract RuleModel build();
+    }
+}
