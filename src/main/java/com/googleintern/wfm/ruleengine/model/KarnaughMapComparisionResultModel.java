968c9cc55450c2e1500620204f4bf8014b8bfed0
Vivian Han <qintonghan@google.com>
2020-07-02 19:12:40 +0000

    Add KarnaughMapReduction

---
+++ src/main/java/com/googleintern/wfm/ruleengine/model/KarnaughMapComparisionResultModel.java
@@ -0,0 +1,28 @@
+package src.main.java.com.googleintern.wfm.ruleengine.model;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.List;
+import java.util.Set;
+
+@AutoValue
+public abstract class KarnaughMapComparisionResultModel {
+  public abstract ImmutableList<ImmutableList<Integer>> minimizedResults();
+
+  public abstract ImmutableSet<ImmutableList<Integer>> minimizedTerms();
+
+  public static Builder builder() {
+    return new AutoValue_KarnaughMapComparisionResultModel.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setMinimizedResults(List<ImmutableList<Integer>> minimizedResults);
+
+    public abstract Builder setMinimizedTerms(Set<ImmutableList<Integer>> minimizedTerms);
+
+    public abstract KarnaughMapComparisionResultModel build();
+  }
+}
