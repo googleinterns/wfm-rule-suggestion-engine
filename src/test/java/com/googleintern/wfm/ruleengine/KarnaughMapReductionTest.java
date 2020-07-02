a482d05139c23d1a688edbdc4ec7fa42c71ec85e
Vivian Han <qintonghan@google.com>
2020-07-02 21:49:31 +0000

    Add tests for KarnaughMapReduction class.

---
+++ src/test/java/com/googleintern/wfm/ruleengine/KarnaughMapReductionTest.java
@@ -0,0 +1,10 @@
+package src.test.java.com.googleintern.wfm.ruleengine;
+
+import org.junit.Test;
+
+public class KarnaughMapReductionTest {
+    @Test
+    public void minimizeKMapTermsTest(){
+
+    }
+}
